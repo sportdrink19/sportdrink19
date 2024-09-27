294197b14f11617d5bccaff8b34468b4b9a0e9ed
sportdrink19 <>
2024-09-27 16:27:20 -0400

    forgot to save file

--- NeetCode IO Python/Core Skills/Implement Data Structures/Design Dynamic Array (Resizeable Array)/designDynamicArray.py
+++ NeetCode IO Python/Core Skills/Implement Data Structures/Design Dynamic Array (Resizeable Array)/designDynamicArray.py
@@ -3,6 +3,7 @@ class DynamicArray:
     def __init__(self, capacity: int):
         self.dynamicArray = []
         self.capacity = capacity
+        self.length = 0
 
     def get(self, i: int) -> int:
         return self.dynamicArray[i]
@@ -11,31 +12,55 @@ class DynamicArray:
         self.dynamicArray[i] = n
 
     def pushback(self, n: int) -> None:
-        if(self.capacity == len(self.dynamicArray)):
+        if(self.capacity == self.length):
             self.resize()
         self.dynamicArray.append(n)
+        self.length += 1
 
     def popback(self) -> int:
+        self.length -= 1
         return self.dynamicArray.pop()
  
     def resize(self) -> None:
         self.capacity *= 2
 
     def getSize(self) -> int:
-        return len(self.dynamicArray)
+        return self.length
         
     def getCapacity(self) -> int:
         return self.capacity
     
 
-array = DynamicArray(4)
-print(array)
-array.pushback(1)
-print(array)
-array.set(0, 0)
-array.pushback(2)
-print(array)
-print(array.get(0), array.get(1))
-print(array.getCapacity())
-array.popback()
-print(array)
+# Unsure why len() was not working and needed a self managed length variable
+# class DynamicArray:
+
+#     dynamicArray = []
+#     capacity = 0
+
+#     def __init__(self, capacity: int):
+#         if(capacity > 0):
+#             self.capacity = capacity
+
+#     def get(self, i: int) -> int:
+#         return self.dynamicArray[i]
+
+#     def set(self, i: int, n: int) -> None:
+#         self.dynamicArray[i] = n
+
+#     def pushback(self, n: int) -> None:
+#         if(self.capacity == len(self.dynamicArray)):
+#             self.resize()
+            
+#         self.dynamicArray.append(n)
+
+#     def popback(self) -> int:
+#         return self.dynamicArray.pop()
+ 
+#     def resize(self) -> None:
+#         self.capacity *= 2
+
+#     def getSize(self) -> int:
+#         return len(self.dynamicArray)
+        
+#     def getCapacity(self) -> int:
+#         return self.capacity
